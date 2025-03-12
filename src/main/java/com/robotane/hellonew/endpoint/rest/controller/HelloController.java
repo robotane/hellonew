79325f82c1250aeefe1f9a4747855ac9899e7321
John Robotane <johnrobotane@gmail.com>
2025-03-12 11:46:09 +0400

    created HelloService

--- src/main/java/com/robotane/hellonew/endpoint/rest/controller/HelloController.java
+++ src/main/java/com/robotane/hellonew/endpoint/rest/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.robotane.hellonew.endpoint.rest.controller;
 
+import com.robotane.hellonew.service.HelloService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloController {
+  private final HelloService service;
 
   @GetMapping("/hello")
-  public String helloWorld() {
-    return "Hello Again!";
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
   }
 }
---
+++ src/main/java/com/robotane/hellonew/service/HelloService.java
@@ -0,0 +1,35 @@
+package com.robotane.hellonew.service;
+
+import static java.io.File.createTempFile;
+
+import com.robotane.hellonew.file.bucket.BucketComponent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class HelloService {
+  private final BucketComponent bucketComponent;
+
+  @SneakyThrows
+  public String uploadHelloWorldMessage(String name) {
+    var fileSuffix = ".txt";
+    var filePrefix = "hello-world-" + name;
+    var bucketKey = filePrefix + fileSuffix;
+    var fileToUpload = createTempFile(filePrefix, fileSuffix);
+    writeMessageIntoFile("Hello Again " + name + " !", fileToUpload);
+    bucketComponent.upload(fileToUpload, bucketKey);
+    return bucketComponent.presign(bucketKey, Duration.ofMinutes(5)).toString();
+  }
+
+  private void writeMessageIntoFile(String message, File file) throws IOException {
+    FileWriter writer = new FileWriter(file);
+    writer.write(message);
+    writer.close();
+  }
+}
